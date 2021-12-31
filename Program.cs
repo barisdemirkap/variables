7fb0d3a1d6f72d4a28154c1c7d9f17c746c18482
barisdemirkap <barisdemirkap@gmail.com>
2021-12-31 12:23:35 +0300

    initial commit of Variables task

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace operators
+{
+   class Program
+   {
+       static void Main(string[] args)
+       {
+           //atama ve islemli atama
+           int x= 3;
+           int y= 3 ;
+           y=y+2 ;
+            Console.WriteLine(y);
+           y +=2;
+            Console.WriteLine(y);
+           y/=1 ;
+           Console.WriteLine(y);
+           x*=2 ;
+           Console.WriteLine(x);
+
+           //Mantiksal Operatorler
+           // ||,&&, !
+
+           bool isSuccess = true;
+           bool isCompleted = false ;
+
+           if(isSuccess&&isCompleted)
+                Console.WriteLine("Perfect!!");
+           if(isSuccess||isCompleted)
+                Console.WriteLine("Great!!");
+           if(isSuccess&& !isCompleted)
+                Console.WriteLine("Fine!!");
+
+
+                 
+
+       } 
+
+
+   }
+
+}
